e0662e64598d4da57f1aa9f6a017333a9e9b895c
uupaa <uupaa.js@gmail.com>
2012-12-14 23:11:00 +0900

    fix

--- js/logic.json.query.js
+++ js/logic.json.query.js
@@ -6,7 +6,7 @@
 
 // --- header ----------------------------------------------
 function _defineLibraryAPIs() {
-    JSON.query = JSON_query;
+    JSON.x_query = JSON_query;
 }
 
 /*
@@ -18,9 +18,9 @@ function _defineLibraryAPIs() {
             lv2_a: {}
         }
     }
-    JSON.query(json, /^lv1_a$/) -> [ { depth: 1, object: lv1_a, path: "/lv1_a" } ]
-    JSON.query(json, /^lv2_a$/) -> [ { depth: 2, object: lv2_a, path: "/lv1_a/lv2_a" },
-                                     { depth: 2, object: lv2_a, path: "/lv1_b/lv2_a" }]
+    JSON.query(json, /^lv1_a$/) -> [ { object: lv1_a, path: ["lv1_a"] } ]
+    JSON.query(json, /^lv2_a$/) -> [ { object: lv2_a, path: ["lv1_a", "lv2_a"] },
+                                     { object: lv2_a, path: ["lv1_b", "lv2_a"] }]
  */
 // --- library scope vars ----------------------------------
 
@@ -28,25 +28,24 @@ function _defineLibraryAPIs() {
 function JSON_query(json,       // @arg Object:
                     selector,   // @arg RegExp(= ""): /^key$/
                     maxDepth) { // @arg Integer(= 0): max depth
-                                // @ret ObjectArray: [ { object, path, depth }, ...]
+                                // @ret ObjectArray: [ { object, path }, ...]
                                 //      object - Object: matched Object
-                                //      path - String: matched Object path. "a.b.c.d"
-                                //      depth - Integer: matchd Object depth. from 0
+                                //      path - StringArray: matched Object path. [key, ...]
                                 // @desc: find object
     selector = selector || /^\w+$/;
     maxDepth = maxDepth || 0;
     var rv = [];
 
-    return _recursiveFind(json, "", 0);
+    return _recursiveFind(json, [], 0);
 
     function _recursiveFind(obj, path, depth) {
         for (var key in obj) {
             if (selector.test(key)) {
-                rv.push( { object: obj[key], path: path + "/" + key, depth: depth + 1 } );
+                rv.push( { object: obj[key], path: path.concat(key) } );
             }
             if (mm.type(obj[key]) === "object") {
-                if (maxDepth < depth + 1) {
-                    _recursiveFind(obj[key], path + "/" + key, depth + 1);
+                if (!maxDepth || maxDepth < depth + 1) {
+                    _recursiveFind(obj[key], path.concat(key), depth + 1);
                 }
             }
         }
