2289d45b365bd15be4037a22603c132dce38a914
uupaa <uupaa.js@gmail.com>
2013-01-04 03:45:30 +0900

    split polyfill.es.js

--- js/polyfill.es5.js
+++ js/polyfill.es5.js
@@ -1,25 +1,20 @@
-// es.js: polyfill ECMAScript 262-3rd and 5th method and properties
+// polyfill.es5.js: polyfill ECMAScript 262-3rd and 5th method and properties
 
 //{@es
-(function(global) { // @arg Global: window or global
+(function(global) {
 
 function _polyfill() {
     if (!Object.keys) {
          Object.keys = Object_keys;     // Object.keys(obj:Mix):Array
     }
-
 //{@ie8
     if (!Object.defineProperties) {
          Object.__defineProperty__ = Object.defineProperty; // keep original
          Object.defineProperty = Object_defineProperty; // Object.defineProperty
     }
     if (!Object.freeze) {
-         Object.freeze = function(obj) {
-         };
+         Object.freeze = function(obj) {};
     }
-//}@ie8
-
-//{@ie8
     // --- force override Date#toJSON result ---
     // [IE8] (original) "2012-09-16T21:53:39Z"
     //       (fix to)   "2012-09-16T21:53:39.000Z" (supply Milliseconds)
@@ -28,7 +23,7 @@ function _polyfill() {
     }
 //}@ie8
 
-    // --- ES5, ES6 Methods ---
+    // --- ES5 Methods ---
     wiz(Date, {
         now:        Date_now            // Date.now():Integer
     });
@@ -36,8 +31,6 @@ function _polyfill() {
         toJSON:     Date_toJSON         // Date#toJSON():JSONObject
     });
     wiz(Array, {
-        of:         Array_of,           // Array.of(...:Mix):Array
-        from:       Array_from,         // Array.from(list:FakeArray):Array
         isArray:    Array_isArray       // Array.isArray(mix:Mix):Boolean
     });
     wiz(Array.prototype, {
@@ -52,15 +45,7 @@ function _polyfill() {
         reduceRight:Array_reduceRight   // [].reduceRight(fn:Function, init:Mix):Mix
     });
     wiz(String.prototype, {
-        trim:       String_trim,        // "".trim():String
-        repeat:     String_repeat,      // "".repeat(count:Integer):String
-        reverse:    String_reverse      // "".reverse():String
-    });
-    wiz(Number, {
-        isNaN:      Number_isNaN,       // Number.isNaN(mix:Mix):Boolean
-        isFinite:   Number_isFinite,    // Number.isFinite(mix:Mix):Boolean
-        isInteger:  Number_isInteger,   // Number.isInteger(mix:Mix):Boolean
-        toInteger:  Number_toInteger    // Number.toInteger(mix:Mix):Integer
+        trim:       String_trim         // "".trim():String
     });
     wiz(Function.prototype, {
         bind:       Function_bind       // Function#bind():Function
@@ -100,7 +85,7 @@ function Object_defineProperty(obj,          // @arg Object:
                                              //              configurable }
                                              // @help: Object.defineProperty
     if (obj.nodeType) {
-        Object.__defineProperty__(obj, prop, descriptor);
+        Object.__defineProperty__(obj, prop, descriptor); // call original
         return;
     }
 
@@ -111,7 +96,7 @@ function Object_defineProperty(obj,          // @arg Object:
     "set"   in descriptor && (type |= 0x4); // accessor descriptor
 
     if (type & 0x1 && type & 0x6) {
-        throw new TypeError("BAD_ARG");
+        throw new TypeError("BAD_DESCRIPTOR");
     }
     type & 0x1 && (obj[prop] = descriptor.value);
     type & 0x2 && obj.__defineGetter__(prop, descriptor.get);
@@ -119,7 +104,6 @@ function Object_defineProperty(obj,          // @arg Object:
 }
 //}@ie8
 
-// --- ES5, ES6 --------------------------------------------
 function Date_now() { // @ret Integer: milli seconds
                       // @desc: get current time
                       // @help: Date.now
@@ -285,24 +269,6 @@ function String_trim() { // @ret String:
                 replace(/\s+$/, "");
 }
 
-function Array_of(ooo) { // @var_args Mix: values
-                         // @ret MixArray:
-                         // @desc: Array.of(1, 2, 3) -> [1, 2, 3]
-                         // @help: Array.of
-    return Array.prototype.slice.call(arguments);
-}
-
-function Array_from(fakeArray) { // @arg FakeArray: Arguments or NodeList
-                                 // @ret Array/NodeArray:
-                                 // @help: Array.from
-    var rv = [], i = 0, iz = fakeArray.length;
-
-    for (; i < iz; ++i) {
-        rv.push(fakeArray[i]);
-    }
-    return rv;
-}
-
 function Date_toJSON() { // @ret String: "2000-01-01T00:00:00.000Z"
                          // @help: Date#toJSON
     var dates = { y:  this.getUTCFullYear(),         // 1970 -
@@ -319,56 +285,6 @@ function Date_toJSON() { // @ret String: "2000-01-01T00:00:00.000Z"
                            (times.m < 10 ? "0" : "") + times.m + ":" +
                            (times.s < 10 ? "0" : "") + times.s + "." +
                            ("00" + times.ms).slice(-3) + "Z";
-}
-
-function Number_isNaN(mix) { // @arg Mix:
-                             // @ret Boolean:
-                             // @help: Number.isNaN
-    return typeof mix === "number" && global.isNaN(mix);
-}
-
-function Number_isFinite(mix) { // @arg Mix:
-                                // @ret Boolean:
-                                // @help: Number.isFinite
-    return typeof mix === "number" && global.isFinite(mix);
-}
-
-function Number_isInteger(mix) { // @arg Mix:
-                                 // @ret Boolean:
-                                 // @desc: is integer
-                                 // @help: Number.isInteger
-    return typeof mix === "number" && global.isFinite(mix) &&
-                  mix > -0x20000000000000 &&
-                  mix <  0x20000000000000 &&
-                  Math.floor(mix) === mix;
-}
-
-function Number_toInteger(mix) { // @arg Mix:
-                                 // @ret Integer:
-                                 // @desc: to integer
-                                 // @help: Number.toInteger
-    var num = +mix;
-
-    if (num !== num) { // isNaN(num)
-        return +0;
-    }
-    if (num === 0 || !global.isFinite(num)) {
-        return num;
-    }
-    return (num < 0 ? -1 : 1) * Math.floor(Math.abs(num));
-}
-
-function String_repeat(count) { // @arg Integer: repeat count. negative is 0
-                                // @ret String: repeated string
-                                // @desc: repeat strings
-                                // @help: String#repeat
-    return (this.length && count > 0) ? Array((count + 1) | 0).join(this) : "";
-}
-
-function String_reverse() { // @ret String:
-                            // @desc: reverse characters
-                            // @help: String#reverse
-    return this.split("").reverse().join("");
 }
 
 function Function_bind(context, // @arg that: context
@@ -390,17 +306,22 @@ function Function_bind(context, // @arg that: context
     return rv;
 }
 
+// --- build -----------------------------------------------
 function wiz(object, extend, override) {
     for (var key in extend) {
-        (override || !(key in object)) && Object.defineProperty(object, key, {
-            configurable: true, writable: true, value: extend[key]
-        });
+        if (override || !(key in object)) {
+            Object.defineProperty(object, key, {
+                configurable: true, writable: true, value: extend[key]
+            });
+        }
     }
 }
 
-// --- export ---
+// --- export ----------------------------------------------
 _polyfill();
 
 })(this.self || global);
 //}@es
+
+// --- test ------------------------------------------------
 
